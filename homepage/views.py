3d263558620bd1ab6fe5d3ba2e33ca8af5502ea5
Tim Hindman <timmyturner45@gmail.com>
2020-08-07 10:57:37 -0400

    first

---
+++ homepage/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from homepage.models import Recipe, Author
+
+def index(request):
+    my_recipes = Recipe.objects.all()
+    return render(request, "index.html", {'recipes': my_recipes} )
+
+def post_detail(request, post_id):
+    my_recipe = Recipe.objects.filter(id=post_id).first()
+    return render(request, "post_detail.html", {"post": my_recipe})
+
+def author_detail(request, auth_id):
+    current_auth = Author.objects.filter(id=auth_id).first()
+    return render(request, "author_detail.html", {"auth": current_auth})
